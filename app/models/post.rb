25cdd00cc4ed9aac7777e9013aeafde2df890e29
= <=>
2025-06-27 18:29:41 +0530

    added enum for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  enum :status, {draft: 0, published: 1}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_102829) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_27_125105) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
